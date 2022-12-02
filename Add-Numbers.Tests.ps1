5fb96e2f27812b96038ca4058b36f25f262c26e8
Jason Lange <jason@bitzbyte.com>
2022-12-02 17:06:53 -0500

    Add tests

--- Add-Numbers.Tests.ps1
+++ Add-Numbers.Tests.ps1
@@ -7,3 +7,15 @@ Describe "Add-Numbers" {
         Add-Numbers | Should -Be 0
     }
 }
+
+Describe 'Add-Numbers 1' {
+    It 'Returns 1 when 1 is provided' {
+        Add-Numbers 1 | Should -Be 1
+    }
+}
+
+Describe 'Add-Numbers "1,2"' {
+    It 'Returns 3 when "1,2" is provided' {
+        Add-Numbers '1,2' | Should -Be 3
+    }
+}
