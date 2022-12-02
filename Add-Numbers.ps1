32c7b1d358283e11c0c3b99653c6ce1ade4f3e84
Jason Lange <jason@bitzbyte.com>
2022-12-02 17:18:04 -0500

    Initial attempt at extracting and adding numbers

--- Add-Numbers.ps1
+++ Add-Numbers.ps1
@@ -1,7 +1,16 @@
 function Add-Numbers
 {
     [CmdletBinding()]
-    param()
+    param(
+        [Parameter(Position=1, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string]
+        $InputString = [string]::Empty
+    )
+
+    $SplitStr = [regex]::Split($InputString, '\D+')
+
+    $Sum = 0
+    $Sum += $SplitStr | ForEach-Object { [int] $_ }
 
     # Return 0 when no input is provided
     Write-Output -InputObject 0
